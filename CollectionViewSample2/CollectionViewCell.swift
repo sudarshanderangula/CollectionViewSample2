2060937aa46de8a7a0cfd845215d283af6093982
Sundir Talari <sundirtalari@sundirs-macbook-air.local>
2018-05-15 13:01:25 +0530

    FirstCommitSuccessfully Done

---
+++ CollectionViewSample2/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewSample2
+//
+//  Created by Sundir Talari on 15/05/18.
+//  Copyright © 2018 Sundir Talari. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
---
+++ CollectionViewSample2/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  CollectionViewSample2
+//
+//  Created by Sundir Talari on 15/05/18.
+//  Copyright © 2018 Sundir Talari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+   let items = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+     
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionCell", for: indexPath) as! CollectionViewCell
+        cell.myLabel.text = items[indexPath.row]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+
+
+}
+
